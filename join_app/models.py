4af2ff93d13288403b8eabd2a3f64d6fb54e38f8
phischa <ph.git@mailbox.org>
2025-01-19 22:51:47 +0100

    assigned contacts to tasks

--- join_app/models.py
+++ join_app/models.py
@@ -10,23 +10,22 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
+class Contact(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    phone = PhoneNumberField(unique=True, region="DE", null=True, blank=True, default=None)
+
+    def __str__(self):
+        f"{self.name}, {self.email}"
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=300)
-    #assigned = assignedContacts;
+    assigned = models.ForeignKey(Contact, on_delete=models.CASCADE, null=True, blank=True, default=None)
     date = models.DateField()
     prio = models.BooleanField(default=False)
     #category = 
 
     def __str__(self):
         return self.title
-
-class Contact(models.Model):
-    name = models.CharField(max_length=25, unique=True)
-    email = models.EmailField(max_length=255, unique=True)
-    phone = PhoneNumberField(unique=True, region="DE", null=True, blank=True)
-
-    def __str__(self):
-        f"{self.name}, {self.email}"
